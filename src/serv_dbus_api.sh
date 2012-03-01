0cd8011ead26a65551480b4b5b8dc981dbd91506
Elvis Pfützenreuter <epx@signove.com>
2012-03-01 00:04:08 -0300

    Added Doxygen-excluding comments to D-Bus .h generator

--- src/serv_dbus_api.sh
+++ src/serv_dbus_api.sh
@@ -1,4 +1,21 @@
+doxy ()
+{
+echo '/**                        ' >> tmppiggy
+echo ' * \cond Undocumented     ' >> tmppiggy
+echo ' */                        ' >> tmppiggy
+echo >> tmppiggy
+
+cat $1 >> tmppiggy
+
+echo '/**              ' >> tmppiggy
+echo ' * \endcond     ' >> tmppiggy
+echo ' */              ' >> tmppiggy
+
+mv -f tmppiggy $1
+}
+
 dbus-binding-tool --mode=glib-server --prefix=srv serv_dbus_api.xml > serv_dbus_api.h
 dbus-binding-tool --mode=glib-server --prefix=device serv_dbus_api_device.xml > serv_dbus_api_device.h
 
-# FIXME add cond Undocumented fence 
+doxy serv_dbus_api.h
+doxy serv_dbus_api_device.h
