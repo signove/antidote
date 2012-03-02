0763e112fbd302e015a91e8d2c1f4f9fcaccf259
Elvis Pfutzenreuter <epx@epx.com.br>
2012-03-01 21:57:06 -0300

    Added glucose meter specialization in Android build

--- src/specializations/Android.mk
+++ src/specializations/Android.mk
@@ -7,7 +7,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) $(LOCAL_PATH)/.. $(LOCAL_PATH)/../..
 
 LOCAL_SRC_FILES = blood_pressure_monitor.c \
 		   pulse_oximeter.c \
-		   weighing_scale.c
+		   weighing_scale.c \
+		   glucometer.c
 
 LOCAL_MODULE:= libantidotespecializations
 LOCAL_MODULE_TAGS := debug eng
