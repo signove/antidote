82aec685ce6db8d91dd76709d0fe92bead27725d
Elvis Pfützenreuter <epx@signove.com>
2012-03-27 23:26:00 -0300

    Added automatic binary choice for grind.sh

--- src/grind.sh
+++ src/grind.sh
@@ -4,4 +4,10 @@ export G_SLICE=always-malloc
 export G_DEBUG=gc-friendly
 VALGRIND_CMD="valgrind --suppressions=../tests/resources/memcheck.supp --error-exitcode=1 --tool=memcheck --leak-check=full"
 
-$VALGRIND_CMD .libs/lt-healthd $*
+if [ -e .libs/lt-healthd ]; then
+	EXE=.libs/lt-healthd
+else
+	EXE=./healthd
+fi
+
+$VALGRIND_CMD $EXE $*
